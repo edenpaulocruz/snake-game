651eb7ef123fe35ba68e17469204b759ea6488f0
edenpaulocruz <edenpaulo@gmail.com>
2021-03-02 22:29:19 -0300

    Definição dos movimentos da cobrinha

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32
 let snake = [];
+let direction = "right";
 
 snake[0] = {
   x: 8 * box,
   y: 8 * box
-}
+};
 
 function createBG() {
   context.fillStyle = "lightgreen";
@@ -20,5 +21,26 @@ function createSnake() {
   }
 }
 
-createBG();
-createSnake();
+function startGame() {
+  createBG();
+  createSnake();
+
+  let snakeX = snake[0].x
+  let snakeY = snake[0].y
+
+  if(direction == "right") snakeX += box;
+  if(direction == "left") snakeX -= box;
+  if(direction == "up") snakeY -= box;
+  if(direction == "down") snakeY += box;
+
+  snake.pop();
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY
+  }
+
+  snake.unshift(newHead);
+}
+
+let game = setInterval(startGame, 100);

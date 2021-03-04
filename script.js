ea7c974ae234dad7bd65431c8e1e9c707ae613f5
edenpaulocruz <edenpaulo@gmail.com>
2021-03-03 21:29:42 -0300

    Refatorando e corrigindo um bug

--- script.js
+++ script.js
@@ -33,12 +33,18 @@ function drawFood() {
 }
 
 function startGame() {
-  updateScore();
-  
-  if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
-  if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
-  if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
-  if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+  if(snake[0].x > 15 * box &&
+    (direction == "right" || direction == "up" || direction == "down")) 
+    snake[0].x = 0;
+  if(snake[0].x < 0 &&
+    (direction == "left" || direction == "up" || direction == "down"))
+    snake[0].x = 16 * box;
+  if(snake[0].y > 15 * box &&
+    (direction == "down" || direction == "right" || direction == "left"))
+    snake[0].y = 0;
+  if(snake[0].y < 0 &&
+    (direction == "up"  || direction == "right" || direction == "left"))
+    snake[0].y = 16 * box;
 
   for(i = 1; i < snake.length; i++) {
     if(snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
@@ -63,6 +69,7 @@ function startGame() {
     snake.pop();
   } else {
     points++;
+    SCORE.innerText = points;
     food.x = Math.floor(Math.random() * 15 + 1) * box;
     food.y = Math.floor(Math.random() * 15 + 1) * box;
   }
@@ -80,10 +87,6 @@ function update(event) {
   if(event.keyCode == 38 && direction != "down") direction = "up";
   if(event.keyCode == 39 && direction != "left") direction = "right";
   if(event.keyCode == 40 && direction != "up") direction = "down";
-}
-
-function updateScore() {
-  SCORE.innerText = points;
 }
 
 let game = setInterval(startGame, 100);
